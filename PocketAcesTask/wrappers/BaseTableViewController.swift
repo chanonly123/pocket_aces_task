e8fb6ed02e2877d58fbf96784cb81d0d0bc68335
Chandan Karmakar <chandan@eezylife.com>
2020-04-03 16:27:54 +0530

    working in headlines

---
+++ PocketAcesTask/wrappers/BaseTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BaseTableViewController.swift
+//  PocketAcesTask
+//
+//  Created by Chandan Karmakar on 03/04/20.
+//  Copyright © 2020 chanonly123. All rights reserved.
+//
+
+import Foundation
